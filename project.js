03c3a4dd9ddce71ac6c7625dea5b961f3205a4db
Ishita Tiwari <ishitatiwari060@gmail.com>
2026-01-30 10:49:05 +0530

    history recorded

--- project.js
+++ project.js
@@ -32,19 +32,56 @@ function display(products){
         });
 }
 // by using button
-// let btn=document.getElementById("btn")
-// btn.addEventListener("click",()=>{
-//     let val=document.getElementById("box").value.toLowerCase();
+let btn=document.getElementById("btn")
+btn.addEventListener("click",()=>{
+    // save to local storage
+    let val=document.getElementById("box").value.trim().toLowerCase();
+    let filterProducts=products.filter((product)=>{
+        return product.title.toLowerCase().includes(val);
+    },0)
+    if(val==="")  return;
+    let history=JSON.parse(localStorage.getItem("searchHistory")) || [];
+    let exists = history.some(item => item.query === val);
+    if(!exists){
+        history.push({
+            query:val,
+            time:Date.now()
+        });
+        localStorage.setItem("searchHistory",JSON.stringify(history));  
+    }
+    display(filterProducts);
+})
+// by using input
+// document.getElementById("box").addEventListener("input",(e)=>{
+//     let val=e.target.value.toLowerCase();
 //     let filterProducts=products.filter((product)=>{
 //        return product.title.toLowerCase().includes(val);
 //     },0)
 //     display(filterProducts);
 // })
-// by using input
-document.getElementById("box").addEventListener("input",(e)=>{
-    let val=e.target.value.toLowerCase();
-    let filterProducts=products.filter((product)=>{
-       return product.title.toLowerCase().includes(val);
-    },0)
-    display(filterProducts);
+
+const suggestionBox=document.getElementById("suggestion")
+let search=document.getElementById("box")
+search.addEventListener("input",()=>{
+    console.log("suggestion working");
+    const text=search.value.toLowerCase();
+    const history=JSON.parse(localStorage.getItem("searchHistory")) || [];
+    if (text === "") {
+    suggestionBox.innerHTML = "";
+    return;
+    }
+    const matches=history.filter(item=>
+        item.query.toLowerCase().includes(text)
+    )
+    suggestionBox.innerHTML="";
+    matches.forEach(item=>{
+        const div=document.createElement("div");
+        div.className="suggestion-item";
+        div.innerText=item.query
+        div.addEventListener("click", () => {
+            search.value = item.query;
+            btn.click();          // reuse search logic
+            suggestionBox.innerHTML = "";
+        });
+    })
 })
